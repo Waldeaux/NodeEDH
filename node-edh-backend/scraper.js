1764c8b11e0238532fcd7b945124d4e7f9f98616
Waldeaux <tswalden95@gmail.com>
2021-06-08 07:54:39 -0500

    Add dynamic insert modal
    Should allow dynamically determining dfcs

--- node-edh-backend/scraper.js
+++ node-edh-backend/scraper.js
@@ -12,6 +12,7 @@ async function main(){
     //await testInsertScrape();
     await insertScraping();
     //await getCardsById();
+    console.log(query);
     await queryDb();
 }
 async function queryDb(){
@@ -150,7 +151,7 @@ async function insertScraping(){
     await page.click('#wizardCookieBannerOptOut');
     const newPage = await newPagePromise;
     await newPage.close();
-    await page.type('#autoCompleteSourceBoxsetAddText0_InnerTextBox', 'School of Mages');
+    await page.type('#autoCompleteSourceBoxsetAddText0_InnerTextBox', 'Kaldheim');
     await page.click('#ctl00_ctl00_MainContent_Content_setAdd');
     await Promise.all([
         page.waitForNavigation(),
@@ -176,6 +177,15 @@ async function insertScraping(){
             const multiverseId = Number.parseInt(result.split('=')[1]);
             let layoutAppend = '';
             let name = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_subtitleDisplay`);
+            let faceName = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=nameRow] .value`);
+            if(name != faceName){
+            await Promise.all([
+                page.waitForNavigation(),
+                page.goBack()
+            ]);
+                x++;
+                continue;
+            }
             let layoutTest = await page.$(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ArtistCredit`);
             if(!layoutTest){
                 let firstVariationLinkIds = await page.$$eval(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 a.variationLink`, nodes => nodes.map(n => n.id));
@@ -185,8 +195,7 @@ async function insertScraping(){
                 });
                 if(isFront)
                 {
-                    let faceName = await getInnerContent(`[id*=rightCol] [id*=nameRow] .value`);
-                    console.log(faceName);
+                    await dynamicInsertModalCard();
                 }
             }
             await Promise.all([
@@ -348,6 +357,7 @@ async function insertScraping(){
     page.close();
 }
 
+
 async function insertModalCard(firstAppend, secondAppend){
     const url = page.url();
     const regex = /\bmultiverseid=\b[0-9]+[?]?/;
@@ -437,6 +447,7 @@ async function insertModalCard(firstAppend, secondAppend){
     let subtypeString = subtypes.join(',');
     let flavorText = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent${firstAppend}_FlavorText .value`);
     let rarity = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent${firstAppend}_rarityRow .value span`);
+    
     let setCode = 'KHM';
     let standardized_name = faceName.replace(/[',\-]*/g, '');
 
@@ -512,6 +523,177 @@ async function insertModalCard(firstAppend, secondAppend){
     standardized_name]);
     return Promise.resolve();
 }
+
+async function dynamicInsertModalCard(){
+    const url = page.url();
+    const regex = /\bmultiverseid=\b[0-9]+[?]?/;
+    const result = url.match(regex)[0];
+    const multiverseId = Number.parseInt(result.split('=')[1]);
+    let layoutAppend = '';
+    let faceName = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=nameRow] .value`);
+    let backName = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent1 [id*=nameRow] .value`);
+    let artist = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=ArtistCredit] a`);
+    let cmc = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=cmcRow] .value`);
+    let number = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=numberRow] .value`);
+    let type = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=typeRow] .value`)
+    let imageChildren = await page.$$eval(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=textRow] img`, nodes => nodes.map(n => n.alt));
+    imageChildren = imageChildren.map(x => symbolSwitch(x));
+    let text = await page.$$eval(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=textRow] .cardtextbox`, nodes => nodes.map(n => n.innerHTML));
+    let textString= text.join('\n');
+    for(let x = 0; x < imageChildren.length; x++){
+        textString = textString.replace(/<img[ A-Za-z0-9?;&="\/.]+>/, `{${imageChildren[x]}}`);
+    }
+    let backText = await page.$$eval(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=textRow] .cardtextbox`, nodes => nodes.map(n => n.innerHTML));
+    let backTextString= backText.join('\n');
+    for(let x = 0; x < imageChildren.length; x++){
+        backTextString = backTextString.replace(/<img[ A-Za-z0-9?;&="\/.]+>/, `{${imageChildren[x]}}`);
+    }
+    let manaCost = await page.$$eval(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=manaRow] .value img`, nodes => nodes.map(n => n.alt));
+    let manaCostText = manaCost.map(x =>symbolSwitch(x)
+    ).join('}{');
+    if(manaCostText.length >0){
+        manaCostText = `{${manaCostText}}`
+    }
+    let backManaCost = await page.$$eval(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent1 [id*=manaRow] .value img`, nodes => nodes.map(n => n.alt));
+    let backManaCostText = backManaCost.map(x =>symbolSwitch(x)
+    ).join('}{');
+    if(backManaCostText.length >0){
+        backManaCostText = `{${backManaCostText}}`
+    }
+    let ptType = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=ptRow] .label`);
+
+    let ptText = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=ptRow] .value`);
+    let power = 0;
+    let toughness = 0;
+    let loyalty = 0;
+    if(ptType == 'Loyalty:'){
+        loyalty = ptText;
+    }
+    else{
+        let pt = ptText.split('/');
+        if(pt.length > 1){
+            power = pt[0].trim();
+            toughness = pt[1].trim();
+        }
+    }
+    let typeArray = type.split(/[ ]+/);
+    let typeindex = typeArray.indexOf('—');
+    let subtypesFound = true;
+    let supertypes = [];
+    let subtypes = [];
+    let possibleTypes = ['Land', 'Creature', 'Artifact', 'Planeswalker', 'Sorcery', 'Instant', 'Enchantment', 'Tribal'];
+    let types = [];
+    if(typeindex == -1){
+        typeindex = typeArray.length;
+        subtypesFound = false;
+    }
+    if(typeindex > 0){
+        if(subtypesFound){
+            supertypes = typeArray.slice(0, typeindex);
+        }
+        else{
+            supertypes = typeArray;
+        }
+    }
+    if(subtypesFound){
+        subtypes = typeArray.slice(typeindex + 1, typeArray.length + 1);
+    }
+
+    supertypes.forEach((element, index) => {
+        if(possibleTypes.includes(element)){
+            supertypes.splice(index, 1);
+            types.push(element);
+        }
+    });
+    let typeString = types.join(',');
+    let supertypeString = supertypes.join(',');
+    let subtypeString = subtypes.join(',');
+    let flavorText = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=FlavorText] .value`);
+    let rarity = await getInnerContent(`#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardComponent0 [id*=rarityRow] span`);
+    switch(rarity){
+        case('Mythic Rare'):
+        rarity = 'mythic';
+        break;
+    }
+    console.log(rarity);
+    let setCode = 'KHM';
+    let standardized_name = faceName.replace(/[',\-]*/g, '');
+
+    let colors = [];
+    let colorIdentity = [];
+    let combinedTextString = textString + ' ' + backTextString;
+    let possibleColors = ['W', 'U', 'B', 'R', 'G'];
+    possibleColors.forEach(x =>{
+        if(colorIdentitySearch(x, manaCostText)){
+            colors.push(x);
+        }
+    })
+    let combinedManaCostText = manaCostText + ' ' + backManaCostText;
+    possibleColors.forEach(x =>{
+        if(colorIdentitySearch(x, combinedManaCostText)){
+            colorIdentity.push(x);
+        }
+        else if(colorIdentitySearch(x, combinedTextString)){
+            colorIdentity.push(x);
+        }
+    })
+    let colorString = colors.join(',');
+    let colorIdentityString = colorIdentity.join(',');
+
+    let combinedName = faceName + ' // ' + backName;
+
+    //textString = textString.replace(/'/g, `\\\'`);
+    query = " INSERT INTO cards"
+    + "(artist,"
+    + "colorIdentity,"
+    + "colors,"
+    + "convertedManaCost,"
+    + "flavorText,"
+    + "loyalty,"
+    + "manaCost,"
+    + "multiverseId,"
+    + "faceName,"
+    + "name,"
+    + "number,"
+    + "uuid,"
+    + "power,"
+    + "rarity,"
+    + "setCode,"
+    + "subtypes,"
+    + "supertypes,"
+    + "text,"
+    + "toughness,"
+    + "type,"
+    + "types,"
+    + "standardized_name,"
+    + "layout)"
+    + "VALUES ?";
+    queryValues.push([artist,
+    colorIdentityString,
+    colorString,
+    cmc,
+    flavorText,
+    loyalty,
+    manaCostText,
+    multiverseId,
+    faceName,
+    combinedName,
+    number,
+    number+"-" +setCode,
+    power,
+    rarity,
+    setCode,
+    subtypeString,
+    supertypeString,
+    textString,
+    toughness,
+    type,
+    typeString,
+    standardized_name,
+    "modal_dfc"]);
+    return Promise.resolve();
+}
+
 async function getInnerContent(selector){
     let name = '';
     try{
