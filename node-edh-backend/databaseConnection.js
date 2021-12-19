a47beb18befc6d88edd5717cce7a02bbeab38a71
Waldeaux <tswalden95@gmail.com>
2021-12-19 02:05:26 -0600

    Create a singleton database connection
    Created a singleton databaseConnection so that it can be used as needed
    instead of passed as a parameter

---
+++ node-edh-backend/databaseConnection.js
@@ -0,0 +1,26 @@
+
+var mysql = require('mysql');
+var con;
+async function createConnection(){
+    con = mysql.createConnection({
+        host:'nodeedh.cwlxyhjp27tf.us-east-1.rds.amazonaws.com',
+        port:'3306',
+        user:'tswalden95',
+        password:'Tsw1594567533236395!'
+    });
+    var promise = new Promise(resolve => {
+        con.connect(function(error){
+            if(error) throw error;
+            resolve();
+        });
+    }).then(resolve => {
+        return new Promise(resolve => {
+        con.query("use NodeEDH");
+        resolve();
+        })
+    })
+    await promise;
+    return con;
+}
+createConnection();
+module.exports = con;
